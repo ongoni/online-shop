873a25f945d3aac45edadc10628dd710ac066dab
ongoni <ongoni.fg@gmail.com>
2018-07-08 19:55:26 +0400

    add authorization functionality

--- src/main/kotlin/com/ongoni/onlineshop/controller/UserController.kt
+++ src/main/kotlin/com/ongoni/onlineshop/controller/UserController.kt
@@ -1,33 +1,58 @@
 package com.ongoni.onlineshop.controller
 
+import com.ongoni.onlineshop.service.SessionService
 import com.ongoni.onlineshop.service.UserService
+import com.ongoni.onlineshop.utils.HashExtensions
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/users")
-class UserController {
+class UserController : HashExtensions {
     @Autowired
     private lateinit var userService: UserService
+    @Autowired
+    private lateinit var sessionService: SessionService
 
-    @PostMapping
-    fun auth() {
+    @PostMapping(produces = ["application/json"])
+    fun auth(@RequestParam username: String, @RequestParam password: String): Map<String, Any> {
+        val user = userService.findOneByUsername(username)
+        if (!user.isPresent) {
+            return mapOf(
+                    "error" to true,
+                    "message" to "User not found"
+            )
+        }
 
+        if (user.get().password != password.hashed()) {
+            return mapOf(
+                    "error" to true,
+                    "message" to "Incorrect password"
+            )
+        }
+
+        val session = sessionService.create(user.get())
+        sessionService.save(session)
+
+        return mapOf(
+                "error" to false,
+                "access_token" to session.token
+        )
     }
 
-    @GetMapping
-    fun info() {
-
+    @GetMapping(produces = ["application/json"])
+    fun info(@PathVariable id: Long): Map<String, Any> {
+        return userService.findById(id).get().safeSerialized()
     }
 
-    @PutMapping
-    fun update() {
-
+    @PutMapping(produces = ["application/json"])
+    fun update(): Map<String, Any> {
+        return mapOf()
     }
 
-    @DeleteMapping
-    fun delete() {
-
+    @DeleteMapping(produces = ["application/json"])
+    fun delete(@PathVariable id: Long): Map<String, Any> {
+        return mapOf()
     }
 
 }
--- src/main/kotlin/com/ongoni/onlineshop/repository/UserRepository.kt
+++ src/main/kotlin/com/ongoni/onlineshop/repository/UserRepository.kt
@@ -2,9 +2,10 @@ package com.ongoni.onlineshop.repository
 
 import com.ongoni.onlineshop.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface UserRepository : JpaRepository<User, Long> {
 
-    fun findOneByUsername(username: String): User
+    fun findOneByUsername(username: String): Optional<User>
 
 }
